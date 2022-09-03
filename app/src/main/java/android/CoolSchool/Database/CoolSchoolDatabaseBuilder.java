6cdf772d33e651d27a72898f3d36bd38d210492b
AlphaMOVG <jcfalcon820@gmail.com>
2022-09-02 20:51:50 -0500

    commit 5.2

--- app/src/main/java/android/CoolSchool/Database/CoolSchoolDatabaseBuilder.java
+++ app/src/main/java/android/CoolSchool/Database/CoolSchoolDatabaseBuilder.java
@@ -1,12 +1,40 @@
 package android.CoolSchool.Database;
 
+import android.CoolSchool.DAO.AssessmentsDAO;
+import android.CoolSchool.DAO.CoursesDAO;
+import android.CoolSchool.DAO.TermsDAO;
 import android.CoolSchool.Entity.Assessments;
 import android.CoolSchool.Entity.Courses;
 import android.CoolSchool.Entity.Terms;
+import android.content.Context;
 
 import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
+/**
+ * Code that builds the database for the application.
+ * */
 @Database(entities = {Assessments.class, Courses.class, Terms.class}, version = 2, exportSchema = false )
+public abstract class CoolSchoolDatabaseBuilder extends RoomDatabase {
+    public abstract AssessmentsDAO assessmentsDAO();
 
-public class CoolSchoolDatabaseBuilder {
+    public abstract CoursesDAO coursesDAO();
+
+    public abstract TermsDAO termsDAO();
+
+    public static volatile CoolSchoolDatabaseBuilder INSTANCE;
+
+    static CoolSchoolDatabaseBuilder getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (CoolSchoolDatabaseBuilder.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), CoolSchoolDatabaseBuilder.class, "myCoolSchoolDatabase.db")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
