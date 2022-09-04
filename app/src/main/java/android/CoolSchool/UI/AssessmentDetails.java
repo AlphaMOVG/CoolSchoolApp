1cd52c18085366b5ea3ba525be5069571fe81896
AlphaMOVG <jcfalcon820@gmail.com>
2022-09-04 13:08:11 -0500

    commit 7.1

--- app/src/main/java/android/CoolSchool/UI/AssessmentDetails.java
+++ app/src/main/java/android/CoolSchool/UI/AssessmentDetails.java
@@ -4,13 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.CoolSchool.R;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AssessmentDetails extends AppCompatActivity {
+    TextInputEditText assessmentIDTxt;
+    TextInputEditText assessmentNameTxt;
+    TextInputEditText assessmentDatePicker;
+    RadioGroup radioBtnGroup;
+    int id;
+    String name;
+    String date;
+    String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_details);
+        assessmentIDTxt = findViewById(R.id.assessmentIDTxt);
+        assessmentNameTxt = findViewById(R.id.assessmentNameTxt);
+        assessmentDatePicker = findViewById(R.id.assessmentDatePicker);
+        radioBtnGroup = findViewById(R.id.radioBtnGroup);
+        id = getIntent().getIntExtra("id", -1);
+        name = getIntent().getStringExtra("name");
+        date = getIntent().getStringExtra("date");
+        type = getIntent().getStringExtra("type");
     }
 
     /**
