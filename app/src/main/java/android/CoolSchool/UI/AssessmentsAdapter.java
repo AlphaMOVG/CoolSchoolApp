9010cd0a5dc8bf05eb3b6096c7885264715f0b6c
AlphaMOVG <jcfalcon820@gmail.com>
2022-09-04 12:21:58 -0500

    commit 7.0

---
+++ app/src/main/java/android/CoolSchool/UI/AssessmentsAdapter.java
@@ -0,0 +1,81 @@
+package android.CoolSchool.UI;
+
+import android.CoolSchool.Entity.Assessments;
+import android.CoolSchool.R;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AssessmentsAdapter extends RecyclerView.Adapter<AssessmentsAdapter.AssessmentsViewHolder> {
+    class AssessmentsViewHolder extends RecyclerView.ViewHolder {
+        private final TextView assessmentItemView;
+
+        private AssessmentsViewHolder(View itemView) {
+            super(itemView);
+            assessmentItemView = itemView.findViewById(R.id.textView2);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    final Assessments current = mAssessments.get(position);
+                    Intent intent = new Intent(context,AssessmentDetails.class);
+                    intent.putExtra("id", current.getAssessmentsID());
+                    intent.putExtra("name", current.getAssessmentName());
+                    intent.putExtra("date", current.getAssessmentDate());
+                    intent.putExtra("type", current.getAssessmentType());
+                    context.startActivity(intent);
+
+                }
+            });
+
+        }
+    }
+
+    private List<Assessments> mAssessments;
+    private final Context context;
+    private final LayoutInflater mInflater;
+    public AssessmentsAdapter(Context context){
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public AssessmentsAdapter.AssessmentsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View itemView = mInflater.inflate(R.layout.assessment_list_item, parent, false);
+       return new AssessmentsViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AssessmentsAdapter.AssessmentsViewHolder holder, int position) {
+        if(mAssessments != null){
+            Assessments current = mAssessments.get(position);
+            String name = current.getAssessmentName();
+            holder.assessmentItemView.setText(name);
+        }
+        else{
+            holder.assessmentItemView.setText("No assessment name");
+        }
+    }
+
+    public void setAssessments(List<Assessments> assessments){
+        mAssessments = assessments;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mAssessments != null){
+            return mAssessments.size();
+        }
+        else return 0;
+    }
+}
--- app/src/main/java/android/CoolSchool/UI/AssessmentsList.java
+++ app/src/main/java/android/CoolSchool/UI/AssessmentsList.java
@@ -1,11 +1,19 @@
 package android.CoolSchool.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.CoolSchool.Database.Repository;
+import android.CoolSchool.Entity.Assessments;
 import android.CoolSchool.R;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import java.util.List;
 
 public class AssessmentsList extends AppCompatActivity {
 
@@ -13,6 +21,28 @@ public class AssessmentsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessmentslist);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        RecyclerView recyclerView = findViewById(R.id.assessmentRecycler);
+        Repository repo = new Repository(getApplication());
+        List<Assessments> assessments = repo.getAllAssessments();
+        final AssessmentsAdapter adapter = new AssessmentsAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter.setAssessments(assessments);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_termlist, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
