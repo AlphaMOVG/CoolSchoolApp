edf84209aa9e1ac7b57009d068b2352585adad4b
AlphaMOVG <jcfalcon820@gmail.com>
2022-09-04 23:10:04 -0500

    commit 7.4

--- app/src/main/java/android/CoolSchool/UI/AssessmentDetails.java
+++ app/src/main/java/android/CoolSchool/UI/AssessmentDetails.java
@@ -2,8 +2,12 @@ package android.CoolSchool.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.CoolSchool.Database.Repository;
+import android.CoolSchool.Entity.Assessments;
 import android.CoolSchool.R;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -17,6 +21,7 @@ public class AssessmentDetails extends AppCompatActivity {
     String name;
     String date;
     String type;
+    Repository repo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +38,21 @@ public class AssessmentDetails extends AppCompatActivity {
         editAssessmentIDTxt.setText(Integer.toString(id));
         editAssessmentNameTxt.setText(name);
         editAssessmentDatePicker.setText(date);
-        //editRadioBtnGroup.set;
+        //editRadioBtnGroup.setOnCheckedChangeListener(CompoundButton.setOnCheckedChangeListener());
 
 
+    }
+
+    public void saveButton(View view) {
+        Assessments assessments;
+        if(id == -1){
+            int newID = repo.getAllAssessments().get(repo.getAllAssessments().size() - 1).getAssessmentsID() + 1;
+            assessments = new Assessments(newID, editAssessmentNameTxt.getText().toString(), editAssessmentDatePicker.getText().toString(), editRadioBtnGroup);
+            repo.insert(assessments);
+        } else{
+            assessments = new Assessments(id, editAssessmentNameTxt.getText().toString(), editAssessmentDatePicker.getText().toString(), editRadioBtnGroup);
+            repo.update(assessments);
+        }
     }
 
     /**
