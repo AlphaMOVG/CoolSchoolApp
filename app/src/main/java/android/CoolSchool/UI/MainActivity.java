24d6ab0b036ed6c1f0481f9f496aa04d33a0d988
JC Falcon <jcfalcon820@gmail.com>
2022-09-25 17:45:33 -0500

    commit 18.0

--- app/src/main/java/android/CoolSchool/UI/MainActivity.java
+++ app/src/main/java/android/CoolSchool/UI/MainActivity.java
@@ -1,7 +1,5 @@
 package android.CoolSchool.UI;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.CoolSchool.Database.Repository;
 import android.CoolSchool.Entity.Assessments;
 import android.CoolSchool.Entity.Courses;
@@ -10,6 +8,8 @@ import android.CoolSchool.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static int numAlert;
@@ -25,13 +25,13 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         Repository repo = new Repository(getApplication());
 
-        Assessments assessments = new Assessments(1, "Assessment", "10/22/2022", "10/23/2022",1,"This is a test note", 1);
+        Assessments assessments = new Assessments(1, "Test Assessment", "10/22/2022", "10/23/2022",1,"This is a test note", 1);
         repo.insert(assessments);
 
-        Courses courses = new Courses(1,"Blue","10/22/2022", "10/22/2022","Name","512-647-3381","falcon@falcon.com", 1, 1,"This is a test note" );
+        Courses courses = new Courses(1,"Test Course","10/22/2022", "10/22/2022","Name","512-647-3381","falcon@falcon.com", 1, 1,"This is a test note" );
         repo.insert(courses);
 
-        Terms terms = new Terms(1,"Term","10/22/2022","10/22/2022", "this is a test note" );
+        Terms terms = new Terms(1,"Test Term","10/22/2022","10/22/2022", "this is a test note" );
         repo.insert(terms);
     }
 
